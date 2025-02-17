a55f73d9416280a8581d62cf302b5e062c048fde
Matheus Toscano <pteutoscano@gmail.com>
2025-02-17 18:51:51 +0000

    feat: add transparent side effect and changing size of cards

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './style.module.scss';
 import { useGSAP } from '@gsap/react';
 import gsap, { Power4 } from 'gsap';
 
-const CarouselItem = ({ animateEnter, id, animateExit }) => {
+const CarouselItem = ({ animateEnter, id, animateExit, animateState, animateClick }) => {
     const onCursorEnter = () => {
         // stop the carousel
+        animateState('pause')
         // animate up and large width to show informatio
         animateEnter(id)
         console.log('mouse hovered over carousel item')
@@ -13,49 +14,72 @@ const CarouselItem = ({ animateEnter, id, animateExit }) => {
 
     const onCursorExits = () => {
         // revert animation
-        // carousel playing again from where it stopped
         animateExit(id)
+        animateState('resume')
+        // carousel playing again from where it stopped
         console.log('mouse exits carousel item')
     }
 
+    const onCursorClick = () => {
+        animateClick(id)
+        console.log('mouse cliked carousel item')
+    }
+
     return (
-        <div className={styles.CIWrapper} onMouseOver={onCursorEnter} onMouseOut={onCursorExits} />
+        <div className={styles.CIWrapper} onMouseOver={onCursorEnter} onMouseOut={onCursorExits} onClick={onCursorClick}/>
     )
 }
 
 const Carousel = () => {
-    const carouselItemRef = useRef<HTMLInputElement>(null);
+    const [animationState, setAnimationState] = useState('initial');
+    const carouselListRef = useRef<HTMLInputElement>(null);
 
-    const animateCarousel = () => {
-        console.log('starting infinite carousel')
-        gsap.to(`.${styles.carouselList}`, {x: (carouselItemRef.current?.clientWidth - window.innerWidth) * -1, ease: 'none', repeat: -1, duration: 5})
+    const animateEnter = (itemID: number) => {
+        gsap.to([carouselListRef.current?.childNodes[itemID]], { y: -100, ease: Power4.easeOut, width: 420 });
+        gsap.to([carouselListRef.current], { y: -100, ease: Power4.easeOut }).pause();
     }
 
-    const animateEnter = (itemID) => {
-        console.log(`${itemID} animation`)
-        gsap.to([carouselItemRef.current?.childNodes[itemID]], { y: -100, ease: Power4.easeOut });
+    const animateExit = (itemID: number) => {
+        gsap.to([carouselListRef.current?.childNodes[itemID]], { y: 0, ease: Power4.easeInOut, width: 220 });
     }
 
-    const animateExit = (itemID) => {
-        console.log(`${itemID} animation`)
-        gsap.to([carouselItemRef.current?.childNodes[itemID]], { y: 0, ease: Power4.easeInOut });
+    const animateClick = (itemID: number) => {
+        setAnimationState('stop');
+        //gsap.to(carouselListRef.current, { width: '100%',position: 'fixed', top: 0, height: '60%', duration: 1, delay: 1, ease: Power4.easeIn})
+        //gsap.to([carouselListRef.current?.childNodes[itemID]], { width: '100%', duration: 1, delay: 1, ease: Power4.easeIn})
+
     }
 
-    console.log(carouselItemRef);
-
-    useEffect(animateCarousel, []);
+    useGSAP((context) => {
+        let tl = gsap.to(carouselListRef.current, { x: (carouselListRef.current?.clientWidth - window.innerWidth) * -1, ease: 'none', repeat: -1, duration: 6 })
+        if (animationState === 'pause') {
+            tl.pause()
+            context.kill()
+        }
+        if (animationState === 'resume') {
+            tl.resume()
+        }
+        if(animationState === 'stop'){
+            tl.kill()
+            context.kill()
+        }
+        console.log({context})
+    }, [animationState])
 
     return (
         <section className={styles.wrapper}>
-            {carouselItemRef &&
-                <ul className={styles.carouselList} ref={carouselItemRef}>
-                    <CarouselItem id={0} animateEnter={animateEnter} animateExit={animateExit} />
-                    <CarouselItem id={1} animateEnter={animateEnter} animateExit={animateExit} />
-                    <CarouselItem id={2} animateEnter={animateEnter} animateExit={animateExit} />
-                    <CarouselItem id={3} animateEnter={animateEnter} animateExit={animateExit} />
-                    <CarouselItem id={4} animateEnter={animateEnter} animateExit={animateExit} />
+            {carouselListRef &&
+                <ul className={styles.carouselList} ref={carouselListRef}>
+                    <CarouselItem id={0} animateEnter={animateEnter} animateExit={animateExit} animateState={setAnimationState} animateClick={animateClick}/>
+                    <CarouselItem id={1} animateEnter={animateEnter} animateExit={animateExit} animateState={setAnimationState} animateClick={animateClick}/>
+                    <CarouselItem id={2} animateEnter={animateEnter} animateExit={animateExit} animateState={setAnimationState} animateClick={animateClick}/>
+                    <CarouselItem id={3} animateEnter={animateEnter} animateExit={animateExit} animateState={setAnimationState} animateClick={animateClick}/>
+                    <CarouselItem id={4} animateEnter={animateEnter} animateExit={animateExit} animateState={setAnimationState} animateClick={animateClick}/>
+                    <CarouselItem id={5} animateEnter={animateEnter} animateExit={animateExit} animateState={setAnimationState} animateClick={animateClick}/>
+                    <CarouselItem id={6} animateEnter={animateEnter} animateExit={animateExit} animateState={setAnimationState} animateClick={animateClick}/>
                 </ul>
             }
+            <div className={styles.shadeBorderSection}/>
         </section>
     )
 }
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,14 +2,20 @@ import styles from './style.module.scss';
 import gsap from "gsap";
 
 const SearchBar = () => {
-    const onEnter = () => {}
-    const onExit = () => {}
+    const onEnter = () => {
+        gsap.to(`.${styles.wrapper}`, { backgroundColor: 'transparent', scale: 1.025, ease: 'power2.inOut', duration: .3})
+    }
+
+    const onExit = () => {
+        gsap.to(`.${styles.wrapper}`, { backgroundColor: 'white', scale: 1, ease: 'power2.out', duration: .3})
+    }
 
     return(
         <input 
             type="text"
             placeholder="Search episodes..."
             onFocus={onEnter}
+            onBlur={onExit}
             className={styles.wrapper}
         />
     )
